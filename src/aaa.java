99947c79f55e364e8dcfdfcf05053891a2350dc8
l7dcodder <moraaalosh@gmail.com>
2026-05-19 20:29:29 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/aaa.java

--- src/aaa.java
+++ src/aaa.java
@@ -1,6 +1,6 @@
 public class aaa {
     public static void main(String[] args) {
         System.out.println("from ali");
-        System.out.println("3li on top");
+        System.out.println("3li on top1");
     }
 }
